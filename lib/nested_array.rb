c39ec588f6997ae4b5f6c5c7ec4d108991a5f72e
Dominic Chu <dominic.m.chu@gmail.com>
2019-10-13 19:43:50 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -39,11 +39,22 @@ def assembled_matrix
 end
 
 def array_literal_matrix
-  new_array = assembled_matrix
-  new_array.each do |nested|
-    nested.sort
-  end
-  new_array
+  new_array = [
+    [
+      "Don Gately",
+      "Joelle van Dyne",
+      "Pat M.",
+      "Kate Gompert",
+      "Bruce Green"
+    ],
+    [
+      "Hal Incandenza",
+      "Lyle",
+      "Gerhard Schtitt",
+      "Mario Incandenza",
+      "Michael Pemulis"
+    ]
+  ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
