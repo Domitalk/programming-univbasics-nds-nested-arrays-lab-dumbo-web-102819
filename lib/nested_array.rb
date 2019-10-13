50f9e1d3a1debf9bf0be4a5d26a122a449a90869
Dominic Chu <dominic.m.chu@gmail.com>
2019-10-13 19:42:42 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -39,6 +39,11 @@ def assembled_matrix
 end
 
 def array_literal_matrix
+  new_array = assembled_matrix
+  new_array.each do |nested|
+    nested.sort
+  end
+  new_array
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
